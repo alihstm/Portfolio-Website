4456a5503b87fa439e65463885ccef0610d7dd16
alihstm <alishbni2023@gmail.com>
2025-11-03 22:02:39 +0330

    feat: add new resume in "pdf" format

--- portfolio/src/Components/Home.jsx
+++ portfolio/src/Components/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import SocialContact from "./SocialContact";
 import AnimatedBtn from "./Extra/animatedBtn";
-import Resume from "../assets/Material/Resume.png";
+import Resume from "../assets/Material/Resume.pdf";
 
 const typingTexts = [
   "I love coding + coffee",
